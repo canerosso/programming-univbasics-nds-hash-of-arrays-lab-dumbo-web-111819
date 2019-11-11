d2452001583e810474931458fd13b4fa2d22afa8
Cane Rosso <astor@usa.com>
2019-11-11 03:17:26 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,6 +9,6 @@ BASE_HOA = {
 def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
-  BASE_HOA[show] << name
+  BASE_HOA[show] << name # The HoA can be updated by the add_character method adds Snarf to :jetsons
   BASE_HOA[show]
 end
